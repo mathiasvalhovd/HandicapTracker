6987f874e72995ef9f9901f889ed02582ae12638
Fellyla Fiorenza <115087764+felyoww@users.noreply.github.com>
2025-06-03 11:58:05 +0700

    pull request test

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter + Firebase Demo',
+      title: 'HandicapTracker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
